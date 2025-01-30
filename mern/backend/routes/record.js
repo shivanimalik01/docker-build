bd0aa778c4003785e049ff6e1d4e137d90d320c9
shivanimalik01 <shivani.malik@nagarro.com>
2025-01-30 14:04:33 +0530

    changes in build

--- mern/backend/routes/record.js
+++ mern/backend/routes/record.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-// This will help us connect to the database--
+// This will help us connect to the database
 import db from "../db/connection.js";
 
 // This help convert the id from string to ObjectId for the _id.
