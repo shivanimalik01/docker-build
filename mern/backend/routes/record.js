8f985f6c253cb0d30de4f251879bff725f89d051
shivanimalik01 <shivani.malik@nagarro.com>
2025-01-30 12:21:04 +0530

    Initial commit - MERN stack project with Docker

---
+++ mern/backend/routes/record.js
@@ -0,0 +1,118 @@
+import express from "express";
+
+// This will help us connect to the database
+import db from "../db/connection.js";
+
+// This help convert the id from string to ObjectId for the _id.
+import { ObjectId } from "mongodb";
+import redis from "redis";
+
+// Create a Redis client
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URI,
+});
+await redisClient.connect();
+
+// router is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const router = express.Router();
+
+// This section will help you get a list of all the records.
+router.get("/", async (req, res) => {
+
+  // Check if the records are cached in Redis
+
+  const cacheKey = "allRecords";
+
+  const cachedRecords = await redisClient.get(cacheKey);
+
+
+
+  if (cachedRecords) {
+
+    // If cached, send the data from Redis
+
+    console.log("Serving from cache");
+
+    return res.json(JSON.parse(cachedRecords)).status(200);
+
+  }
+
+
+
+  // If not cached, fetch from MongoDB
+  let collection = await db.collection("records");
+  let results = await collection.find({}).toArray();
+
+  // Cache the records in Redis with an expiration of 60 seconds
+
+  await redisClient.setEx(cacheKey, 60, JSON.stringify(results));
+  console.log("Serving from database");
+  res.send(results).status(200);
+});
+
+// This section will help you get a single record by id
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("records");
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// This section will help you create a new record.
+router.post("/", async (req, res) => {
+  try {
+    let newDocument = {
+      name: req.body.name,
+      position: req.body.position,
+      level: req.body.level,
+    };
+    let collection = await db.collection("records");
+    let result = await collection.insertOne(newDocument);
+    res.send(result).status(204);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error adding record");
+  }
+});
+
+// This section will help you update a record by id.
+router.patch("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const updates = {
+      $set: {
+        name: req.body.name,
+        position: req.body.position,
+        level: req.body.level,
+      },
+    };
+
+    let collection = await db.collection("records");
+    let result = await collection.updateOne(query, updates);
+    res.send(result).status(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating record");
+  }
+});
+
+// This section will help you delete a record
+router.delete("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+
+    const collection = db.collection("records");
+    let result = await collection.deleteOne(query);
+
+    res.send(result).status(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting record");
+  }
+});
+
+export default router;
